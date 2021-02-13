68b8c7a00e89ca4b2f5b2c9caa15fc1aeb3428b9
LordBigglesworth <javanpohl@gmail.com>
2021-02-13 14:34:14 -0700

    made the centering a bit more dynamic

--- client/components/search/Search.js
+++ client/components/search/Search.js
@@ -4,8 +4,15 @@ import React from 'react';
 const Search = () => {
   console.log('in Search');
   return (
-    <div className="search">
-      this is search
+    <div id="search" className="centered flex-parent flex-wrap-no flex-align-center flex-center">
+      <div >
+        <div id="search-title" className="center-text">
+          Enter the name of a film or tv show:
+        </div>
+        <div id="search-field" className="center-text">
+          <input type="text" placeholder="Search.."/>
+        </div>
+      </div>
     </div>
   )
 }
