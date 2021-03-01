43eb26cca55d2bc88ab1879243e35d93c18112c6
LordBigglesworth <javanpohl@gmail.com>
2021-03-01 16:01:41 -0700

    onhover flip working, now to add a back side

--- client/components/MoviesM.js
+++ client/components/MoviesM.js
@@ -1,19 +1,27 @@
 import React, { useState, Fragment } from 'react'
 // import Flip from '@material-ui/core/Fade'
-import Flip from 'react-reveal/Flip'
 import Fade from 'react-reveal/Fade'
 import Grid from '@material-ui/core/Grid'
 
 const Movies = ({ movie, onSave, time }) => {
   const poster_url = `https://image.tmdb.org/t/p/w200${movie.poster_path}`
   return (
-    <Flip delay={time}>
-      <Grid item>
-        <div>
-          <img src={poster_url} className="movie-poster"></img>
+    <Grid item>
+      <div className="flip-card">
+        <div className="flip-card-inner">
+          <div className="flip-card-front">
+            <div>
+              <img src={poster_url} className="movie-poster"></img>
+            </div>
+          </div>
+          <div className="flip-card-back">
+            <div>
+              <img src={poster_url} className="movie-poster"></img>
+            </div>
+          </div>
         </div>
-      </Grid>
-    </Flip>
+      </div>
+    </Grid>
   )
 }
 
