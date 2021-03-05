a9cafe447ae8aee9fb97a92b43a5b0948e813576
LordBigglesworth <javanpohl@gmail.com>
2021-03-05 14:56:55 -0700

    change port variable to by dynamic and look first for process.env.PORT

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ const path = require('path');
 const cors = require('cors');
 
 const app = express();
-const PORT = 3002;
+// const PORT = 3002;
+const PORT = process.env.PORT || 3002
 
 // middleware
 const morgan = require('morgan');
