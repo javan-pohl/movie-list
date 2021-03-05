2450174bcc0db40d1155f6903b0d51d907b0668b
LordBigglesworth <javanpohl@gmail.com>
2021-03-05 14:46:21 -0700

    updating heroku info

--- database/db/config.js
+++ database/db/config.js
@@ -1,8 +1,18 @@
 
 module.exports.config = {
-  host : 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'movielist',
+  host : 'us-cdbr-east-03.cleardb.com',
+  user: 'b79f1e9bce6f40',
+  password: '8b99afd9',
+  database: 'heroku_3164bae8ef63aa7',
   insecureAuth : true
 };
+
+// // above for Heroku, below for local
+
+// module.exports.config = {
+//   host : 'localhost',
+//   user: 'root',
+//   password: 'password',
+//   database: 'movielist',
+//   insecureAuth : true
+// };
---
+++ keys/tmdb.js
@@ -0,0 +1,3 @@
+
+
+module.export.api = '69068131cf6aae96cd5fba4cafd706d8'
