f2c52f53986e349c320e257556d354609b5c9e44
LordBigglesworth <javanpohl@gmail.com>
2021-05-08 16:58:02 -0600

    finally got the background image to darken but not the text by applying a linear-gradient

--- client/components/Summary.jsx
+++ client/components/Summary.jsx
@@ -7,20 +7,36 @@ const Summary = ({ movie }) => {
   const poster_url = `https://image.tmdb.org/t/p/w300${movie.poster_path}`
   const backdrop_url = `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
   const backdrop_style = {
-    background: `url(${backdrop_url})`,
-    backgroundSize: "cover"
-  }
+    backgroundImage: `linear-gradient(rgb(0,0,0,0.6),
+      rgb(0,0,0,0.6)), url(${backdrop_url})`,
+    backgroundSize: 'cover'
 
+    // '&::before': {
+    //   content: `''`,
+    //   position: 'absolute',
+    //   top: '0px',
+    //   right: '0px',
+    //   bottom: '0px',
+    //   left: '0px',
+    //   backgroundColor: 'rgba(0, 0 , 0, 0.9)'
+    // }
+  }
 
   return (
     <div className={styles.summary} style={backdrop_style}>
+      {/* // <div className={styles.summary} > */}
       <div className={styles.summaryImgSection}>
         <img className={styles.summaryImg} src={poster_url} />
       </div>
       <div className={styles.summaryBody}>
-        <h1>{movie.original_title}&nbsp;({movie.release_date.slice(0,4)})</h1>
+        <h1>
+          {movie.original_title}&nbsp;({movie.release_date.slice(0, 4)})
+        </h1>
         summary body
       </div>
+      <div
+        style={{ position: 'absolute', WebkitFilter: 'blur(10px) saturate(2)' }}
+      />
     </div>
   )
 }
