7576dfb4f9ef38f0fde91b81a612f0ca6fdbf90a
LordBigglesworth <javanpohl@gmail.com>
2021-05-11 15:55:44 -0600

    added the functions and formatting for the info portion underneath the title in summary

--- client/components/Summary.jsx
+++ client/components/Summary.jsx
@@ -4,30 +4,48 @@ import styles from './Summary.module.css'
 import { getMPAA } from './functions/Functions'
 
 const Summary = ({ movie }) => {
-  console.log('movie: ', movie)
   const MPAA = getMPAA(movie)
-  console.log('MPAA: ', MPAA)
   const poster_url = `https://image.tmdb.org/t/p/w300${movie.poster_path}`
   const backdrop_url = `https://image.tmdb.org/t/p/original${movie.backdrop_path}`
   const backdrop_style = {
     backgroundImage: `linear-gradient(rgb(0,0,0,0.85),
-      rgb(0,0,0,0.85)), url(${backdrop_url})`,
+    rgb(0,0,0,0.85)), url(${backdrop_url})`,
     backgroundSize: 'cover'
   }
-
+  const genres = movie.genres.map(obj => obj.name).join(', ')
+  const runtime = movie.runtime
+  const runtimeStr = `${Math.floor(runtime / 60)}hr ${runtime % 60}m`
+  console.log('runtimeStr: ', runtimeStr)
+  console.log('Summary genres: ', genres)
+  console.log('movie: ', movie)
+  console.log('MPAA: ', MPAA)
+  const renderInfo = () => (
+    <div className={styles.info}>
+      <span className={styles.MPAA}>{MPAA}</span>
+      <span className={styles.infoInner}>
+        {movie.release_date}
+        &nbsp;&#8226;&nbsp;
+        {genres}
+        &nbsp;&#8226;&nbsp;
+        {runtimeStr}
+      </span>
+    </div>
+  )
   return (
     <div className={styles.summary} style={backdrop_style}>
       <div className={styles.summaryImgSection}>
         <img className={styles.summaryImg} src={poster_url} />
       </div>
-      <div className={styles.summaryBody}>
+      <div className={styles.body}>
         <h1>
-          {movie.original_title}&nbsp;<span className={styles.headingDate}>({movie.release_date.slice(0, 4)})</span>
+          {movie.original_title}&nbsp;
+          <span className={styles.headingDate}>
+            ({movie.release_date.slice(0, 4)})
+          </span>
         </h1>
-        <div className={styles.info}></div>
-        summary body
+        {renderInfo()}
+        <div className={styles.overview}>summary body</div>
       </div>
-
     </div>
   )
 }
