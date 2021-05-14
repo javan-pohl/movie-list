9a8f012efed16bc05582c82b30ad3c76c0b323a6
LordBigglesworth <javanpohl@gmail.com>
2021-05-14 17:29:34 -0600

    switched summary preload with prefetch in renderMural()

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -9,12 +9,13 @@ import Login from './Login'
 // const Summary = React.lazy(() => import('./Summary'))
 // const MovieMural = React.lazy(() => import('./MovieMural'))
 // loadable-components method
-const Search = loadable(() => import( './Search'))
-const NavBar = loadable(() => import( './NavBar'))
-const Summary = loadable(() => import( './Summary'))
-const MovieMural = loadable(() => import( './MovieMural'))
+const Search = loadable(() => import('./Search'))
+const NavBar = loadable(() => import('./NavBar'))
+// const Summary = loadable(() => import( './Summary'))
+const MovieMural = loadable(() => import('./MovieMural'))
 // const Search = loadable(() => import(/* webpackPrefetch: true */ './Search'))
 // const NavBar = loadable(() => import(/* webpackPrefetch: true */ './NavBar'))
+let Summary
 // const Summary = loadable(() => import(/* webpackPrefetch: true */ './Summary'))
 // const MovieMural = loadable(() => import(/* webpackPrefetch: true */ './MovieMural'))
 
@@ -131,7 +132,8 @@ function App() {
   }
   function renderMural(thisList) {
     // preload is a function of loadable/component
-    Summary.preload()
+    // Summary.preload()
+    Summary = loadable(() => import(/* webpackPrefetch: true */ './Summary'))
     return (
       <div className="app">
         <MovieMural
