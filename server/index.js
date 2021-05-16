34a1baf54a1321224f232748db19c8ea03a2c38d
LordBigglesworth <javanpohl@gmail.com>
2021-05-15 18:24:37 -0600

    change the server index file to have an if statement for browsers that do not support brotli

--- server/index.js
+++ server/index.js
@@ -22,9 +22,12 @@ app.use(parser.urlencoded({ extended: true }));
 
 // Trying to configure the server to serve compressed assets
 app.get('*.jsx', (req, res, next) => {
-  req.url = req.url + '.br';
-  res.set('Content-Encoding', 'br');
-  res.set('Content-Type', 'application/javascript; charset=UTF-8');
+  if (req.header('Accept-Encoding').includes('br')) {
+    req.url = req.url + '.br';
+    console.log(req.header('Accept-Encoding'));
+    res.set('Content-Encoding', 'br');
+    res.set('Content-Type', 'application/javascript; charset=UTF-8');
+  }
   next();
 });
 // app.use(compression())
